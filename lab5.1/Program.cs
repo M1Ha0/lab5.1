f5e8a35624e710e562075bfba2521af3dc37e04f
MiHa0_o <MiHa0_o@209-8>
2024-12-27 12:02:57 +0200

    Добавьте файлы проекта.

---
+++ lab5.1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+char[,] matrix = new char[5, 4]
+{
+            { '+', 'a', '*', 'b' },
+            { 'c', '+', 'd', '*' },
+            { '*', 'e', '+', 'f' },
+            { 'g', 'h', '*', 'i' },
+            { '+', 'j', 'k', '+' }
+};
+int plusCount = 0;
+int starCount = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        if (matrix[i, j] == '+')
+        {
+            plusCount++;
+        }
+        else if (matrix[i, j] == '*')
+        {
+            starCount++;
+        }
+    }
+}
+Console.WriteLine($"Символ '+' встречается: {plusCount} раз");
+Console.WriteLine($"Символ '*' встречается: {starCount} раз");
